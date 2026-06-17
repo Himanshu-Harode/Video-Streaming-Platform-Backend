5de745cda0648526719af26390c482ef6c96d8f4
Himanshu Harode <harodehimanshu@gmail.com>
2026-06-17 21:57:49 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+ return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => {
       next(err);
     });
@@ -7,12 +7,4 @@ const asyncHandler = (requestHandler) => {
 };
 export { asyncHandler };
 
-// const asyncHandler = (fn) => async (req, res, next) => {
-//   try {
-//     await fn(req,res,next)
-//   } catch (error) {
-//     res
-//       .status(error.code || 500)
-//       .json({ success: false, message: error.message });
-//   }
-// };
+
